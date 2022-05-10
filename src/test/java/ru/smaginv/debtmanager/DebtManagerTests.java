35add78c0bd270d91a46c89042bcdbb61f5a012b
Vitaliy Smagin <rolezz@gmail.com>
2022-05-10 14:35:55 +0300

    initial

---
+++ src/test/java/ru/smaginv/debtmanager/DebtManagerTests.java
@@ -0,0 +1,13 @@
+package ru.smaginv.debtmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DebtManagerTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
