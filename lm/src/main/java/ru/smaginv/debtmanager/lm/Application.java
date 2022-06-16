4cb791abd8ee155e849f2d97cd9b13d37a77a1e0
Vitaliy Smagin <rolezz@gmail.com>
2022-06-16 15:01:57 +0300

    added module lm

---
+++ lm/src/main/java/ru/smaginv/debtmanager/lm/Application.java
@@ -0,0 +1,12 @@
+package ru.smaginv.debtmanager.lm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ lm/src/test/java/ru/smaginv/debtmanager/lm/ApplicationTest.java
@@ -0,0 +1,12 @@
+package ru.smaginv.debtmanager.lm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+}
